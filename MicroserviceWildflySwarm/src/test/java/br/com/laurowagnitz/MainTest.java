b83163204af4bbee523054e2d5721236ba3f3013
Lauro Wagnitz <lauro@agrotis.com>
2018-05-15 22:40:59 -0300

    ajustes

--- MicroserviceWildflySwarm/src/test/java/br/com/laurowagnitz/MainTest.java
+++ MicroserviceWildflySwarm/src/test/java/br/com/laurowagnitz/MainTest.java
@@ -3,45 +3,26 @@ package br.com.laurowagnitz;
 import static org.junit.Assert.assertEquals;
 
 import javax.inject.Inject;
-import javax.ws.rs.client.Client;	
+import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-import org.jboss.arquillian.container.test.api.Deployment;
-import org.jboss.arquillian.junit.Arquillian;
-import org.jboss.shrinkwrap.api.Archive;
-import org.jboss.shrinkwrap.api.ShrinkWrap;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.wildfly.swarm.jaxrs.JAXRSArchive;
-
 import br.com.laurowagnitz.rest.Servico1;
 
-@RunWith(Arquillian.class)
+//@RunWith(Arquillian.class)
+//@DefaultDeployment
 public class MainTest {
 	
 	@Inject
 	Servico1 servico1;
 	
-	@Deployment
-	public static Archive<JAXRSArchive> createDeployment() {
-	    JAXRSArchive deployment = ShrinkWrap.create( JAXRSArchive.class );
-	    try {
-	    	deployment.addPackage("br.com.laurowagnitz.model");
-	    	deployment.addResource(Servico1.class);	    	
-	    } catch (Exception erro) {
-	    	erro.printStackTrace();
-	    }
-	    return deployment;
-	}		
-
-    @Test
+    //@Test
     public void testMyComponent() {    	
     	assertEquals(servico1.consultaVersao().getStatus(), 200);
     }
     
-    @Test
+    //@Test
     public void testMyComponent2() {    	
     	Client client = ClientBuilder.newClient();
     	Response response = client.target("http://localhost:8080").path("/version").request(MediaType.APPLICATION_JSON).get();    	
