707cebc9dd1341399ab56ac2a854fec07275c166
Lauro Wagnitz <lauro.wagnitz@agrotis.com>
2018-05-17 10:21:54 -0300

    inclusao teste autenticacao

---
+++ MicroserviceWildflySwarm/src/main/java/br/com/laurowagnitz/rest/AuthInterceptor.java
@@ -0,0 +1,23 @@
+package br.com.laurowagnitz.rest;
+
+import java.io.IOException;
+
+import javax.annotation.Priority;
+import javax.ws.rs.Priorities;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerRequestFilter;
+import javax.ws.rs.ext.Provider;
+
+@Provider
+@Priority(Priorities.AUTHORIZATION)
+public class AuthInterceptor implements ContainerRequestFilter {
+
+    @Override
+    public void filter(ContainerRequestContext requestContext) throws IOException {
+        if (requestContext.getHeaders().getFirst("authorization") == null) {
+            throw new WebApplicationException("401");
+        }  
+    }
+
+}
